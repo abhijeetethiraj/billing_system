88d49a3ed85894c6d6fe9ea754a77f11d9529387
Prasanna Kamble <prasannamk79@gmzil.com>
2026-07-01 02:21:31 +0530

    Worked on API Service and Exception Handling in case API fails

---
+++ lib/data/api/endpoints.dart
@@ -0,0 +1,9 @@
+class Endpoints {
+  static const String baseUrl = "https://fakestoreapi.com";
+
+  // Gets the master list of all products
+  static const String products = "$baseUrl/products";
+
+  // Gets just the categories
+  static const String categories = "$baseUrl/products/categories";
+}
